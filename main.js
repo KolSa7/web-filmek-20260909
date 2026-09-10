612e247a2d35558be6dea66479be9e8ec63abd19
Kolcze Szabolcs <kolcze.szabolcs@petrik.hu>
2026-09-10 08:49:39 +0200

    Formázás kész

--- main.js
+++ main.js
@@ -107,7 +107,10 @@ filmek.forEach(film => {
 
   const rating = document.createElement("td");
   rating.textContent = film.rating;
+  if (film.rating < 3) {
+    rating.classList.add("low-rating");
+  }
   row.appendChild(rating);
-  
+
   table.appendChild(row);
 });
